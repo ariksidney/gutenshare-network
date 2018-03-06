3a5044cdabc419b8016a3fed47e999fb58a9d970
Arik Sidney Guggenheim <ariksidney@outlook.com>
2018-03-06 19:13:53 +0100

    first commit (spring boot application)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PSIT3-FS18-ZH-5
+# gutenshare network
+
+To start the backend application:
+
+./mvnw clean spring-boot:run -pl gutenshare-web
---
+++ gutenshare-web/src/main/java/com/group4/gutenshareweb/infrastructure/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.group4.gutenshareweb.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping
+    public String getTest() {
+        return "Hello gutenshare network";
+    }
+
+}
