a1bca6de595264bf748658b46ea39aa9f53fc877
Kaspar Wolfisberg <kaspar.wolfisberg@gmail.com>
2018-03-07 01:57:27 +0100

    frontend: user: Add sample component and service

---
+++ gutenshare-frontend/src/app/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { User } from "./user";
+import { ErrorObservable } from "rxjs/observable/ErrorObservable";
+import { catchError } from "rxjs/operators";
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>("/api/user").pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an ErrorObservable with a user-facing error message
+    return new ErrorObservable(
+      'Something bad happened; please try again later.');
+  };
+
+}
---
+++ gutenshare-frontend/src/app/user/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  name: string;
+  surname: string;
+  mail: string;
+  username: string;
+  password: string;
+}
