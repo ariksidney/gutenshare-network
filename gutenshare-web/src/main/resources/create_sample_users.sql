0b5b6f8ee077379b37bd71dae300753ef7de7f0b
Kaspar Wolfisberg <wolfikas@students.zhaw.ch>
2018-03-11 16:00:32 +0100

    web: SQL: Add sample user creatiion script

---
+++ gutenshare-web/src/main/resources/create_sample_users.sql
@@ -0,0 +1,13 @@
+DROP TABLE userTest;
+CREATE TABLE userTest (
+  username varchar(20),
+  name varchar(20),
+  surname varchar(20),
+  email varchar(30)
+);
+
+INSERT INTO userTest (username, name, surname, email) VALUES('AG', 'Guggenheim', 'Arik', 'ag@gutenshare.network');
+INSERT INTO userTest (username, name, surname, email) VALUES('AR', 'Rueegg', 'Alicia', 'ar@gutenshare.network');
+INSERT INTO userTest (username, name, surname, email) VALUES('AS', 'Spasojevic', 'Aleksander', 'as@gutenshare.network');
+INSERT INTO userTest (username, name, surname, email) VALUES('LM', 'Mueller', 'Louis', 'lm@gutenshare.network');
+INSERT INTO userTest (username, name, surname, email) VALUES('KW', 'Wolfisberg', 'Kaspar', 'kw@gutenshare.network');
