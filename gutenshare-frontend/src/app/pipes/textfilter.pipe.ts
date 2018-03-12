0681f3479a47b76e4cc3ff602f97a2ae0a4184dd
Kaspar Wolfisberg <wolfikas@students.zhaw.ch>
2018-03-12 03:15:14 +0100

    frontend: filterpipe: Fix the property mapping

--- gutenshare-frontend/src/app/pipes/textfilter.pipe.ts
+++ gutenshare-frontend/src/app/pipes/textfilter.pipe.ts
@@ -5,18 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class TextfilterPipe implements PipeTransform {
-  transform(items: any[], searchText: string): any[] {
+  transform(items: any[], prop: string, searchText: string): any[] {
     if(!items) return [];
     if(!searchText) return items;
 
     console.log(items);
+    console.log(prop);
     console.log(searchText);
 
     searchText = searchText.toLowerCase();
 
+    if (prop==null) {
+      return items.filter( it => {
+        return it.toLowerCase().includes(searchText);
+      });
+
+    }
 
     return items.filter( it => {
-      return it.name.toLowerCase().includes(searchText);
+      return it[prop].toLowerCase().includes(searchText);
     });
   }
 }
