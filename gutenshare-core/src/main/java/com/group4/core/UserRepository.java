fc25c4bcf9d5d554d1fe4bea584ff2112603c9c6
Arik Sidney Guggenheim <guggeari@students.zhaw.ch>
2018-03-12 19:34:55 +0100

    added api module and much more

---
+++ gutenshare-core/src/main/java/com/group4/core/UserRepository.java
@@ -0,0 +1,10 @@
+package com.group4.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(propagation = Propagation.MANDATORY)
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
