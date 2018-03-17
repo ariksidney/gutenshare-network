d43826a95aae5e3d573dcb2ec1c0ebae06c6cd7a
Arik Sidney Guggenheim <guggeari@students.zhaw.ch>
2018-03-17 01:02:15 +0100

    Revert "added rest api to create document,"
    This reverts commit f7caa5823d05d4d43b3a4d435c49b6687acd0c1d.

--- gutenshare-web/src/main/resources/sql/V1_1_0_0__create_schema.sql
+++ gutenshare-web/src/main/resources/sql/V1_1_0_0__create_schema.sql
@@ -7,14 +7,6 @@ CREATE TABLE T_USER (
   CONSTRAINT PK_USERNAME PRIMARY KEY (username)
 );
 
-CREATE TABLE T_DOCUMENT (
-  document_id       VARCHAR(64) NOT NULL,
-  title             VARCHAR(64) NOT NULL,
-  path_to_file      VARCHAR(200) NOT NULL,
-  filetype          VARCHAR(20) NOT NULL,
-  CONSTRAINT PK_DOCUMENT PRIMARY KEY (document_id)
-);
-
 INSERT INTO T_USER (username, name, surname, mail, password) VALUES('AG', 'Guggenheim', 'Arik', 'ag@gutenshare.network', '123');
 INSERT INTO T_USER (username, name, surname, mail, password) VALUES('AR', 'Rueegg', 'Alicia', 'ar@gutenshare.network', '123');
 INSERT INTO T_USER (username, name, surname, mail, password) VALUES('AS', 'Spasojevic', 'Aleksander', 'as@gutenshare.network', '123');
