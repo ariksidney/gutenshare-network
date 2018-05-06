326a890a68fc3219c0eee3963a2766973790f634
rueegali <rueegali@students.zhaw.ch>
2018-05-06 16:31:38 +0200

    complete browsing and document detail in frontend

--- gutenshare-frontend/src/app/document-detail/document-detail.component.ts
+++ gutenshare-frontend/src/app/document-detail/document-detail.component.ts
@@ -1,163 +1,54 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { DocumentService } from "./document.service";
-import { SCHOOLS, DOCUMENT_TYPES } from '../mock-data/mock-data';
+import {Component, OnInit} from "@angular/core";
+import { SPECIFICDOCUMENT } from '../mock-data/mock-data';
 
 
 @Component({
-  selector: 'app-create-document',
-  templateUrl: './create-document.component.html',
-  styleUrls: ['./create-document.component.scss']
+  selector: 'app-document-detail',
+  templateUrl: './document-detail.component.html',
+  styleUrls: ['./document-detail.component.scss']
 })
 
-export class CreateDocumentComponent implements OnInit {
+export class DocumentDetailComponent implements OnInit {
+  specificDocument: any = SPECIFICDOCUMENT;
+  ratingsForDocumentDetail: any[5] = [];
+  userGaveRating: boolean = false;
 
+  defineRatingForDocumentDetail(rating: number) {
+    for (var i = 1; i <= rating; i++) {
+      this.ratingsForDocumentDetail[i-1] = {
+        "status": "ratingActive",
+        "index": i
+      };
+    }
 
-  schools: any[] = SCHOOLS;
-  documentTypes: string[] = DOCUMENT_TYPES;
-  createDocumentForm: FormGroup;
-  documentType: string;
-  activeSchool: any = null;
-  activeDepartment: any = null;
-  activeCourse: string = null;
-  filterTextInput: string = '';
-  fileName: string = 'Choose file...';
-
-  requiredAlert:string = "This field is required";
-  descriptionAlert:string = "5 to 500 characters required";
-
-  constructor(
-    private fb: FormBuilder,
-    private documentService: DocumentService
-  ) {
-    this.initializeForm();
-  }
+    for (var i = 5; i > (rating); i--) {
+      console.log("i: "+ i);
+      this.ratingsForDocumentDetail[i-1] = {
+        "status": "ratingInactive",
+        "index": i
+      };
+    }
+  };
 
   ngOnInit() {
+    this.defineRatingForDocumentDetail(this.specificDocument.rating);
   }
 
-  pickSchool(school: any): void {
-    this.activeSchool = school;
-    this.createDocumentForm.get('school').setValue(school.name);
-    this.filterTextInput = '';
-  }
-
-  pickDepartment(department: any): void {
-    this.activeDepartment = department;
-    this.createDocumentForm.get('department').setValue(department.name);
-    this.filterTextInput = '';
-  }
-
-  pickCourse(course: any): void {
-    this.activeCourse = course;
-    // todo: what if course too long?
-    this.createDocumentForm.get('course').setValue(course);
-    this.filterTextInput = '';
-  }
-
-  resetSchool(): void {
-    this.activeSchool = null;
-    this.createDocumentForm.get('school').setValue(null);
-    this.resetDepartment();
-  }
-
-  resetDepartment(): void {
-    this.activeDepartment = null;
-    this.createDocumentForm.get('department').setValue(null);
-    this.resetCourse();
-  }
-
-  resetCourse(): void {
-    this.activeCourse = null;
-    this.createDocumentForm.get('course').setValue(null);
-    this.filterTextInput = '';
-  }
-
-  setDocumentType(n : number): void {
-    this.documentType = this.documentTypes[n];
-  }
-
-  setFile(event): void {
-    if(event.target.files.length == 1) {
-      let file = event.target.files[0];
-      this.createDocumentForm.get('file').setValue(file);
-    }
-  }
-
-  postDocument(post): void {
-    let payload = new FormData();
-
-    payload.append('title', post.name);
-    payload.append('documenttype', post.type.toLowerCase());
-    payload.append('document', post.file);
-
-    let prunedTags: string[] = this.pruneArray(post.tags);
-    if (prunedTags.length > 0) {
-      prunedTags.forEach(tag => {
-        payload.append('tags', tag);
-      });
-    }
-
-    if (post.description) {
-      payload.append('description', post.description);
-    }
-    if (post.school) {
-      payload.append('school', post.school);
-    }
-    if (post.department) {
-      payload.append('department', post.department);
-    }
-    if (post.course) {
-      payload.append('course', post.course);
-    }
-
-    this.documentService.addDocument(payload).subscribe(
-      resp => console.log(resp),
+  addComment(commentText: string)
+  {
+    this.specificDocument.comments.push(
+      {
+        "user": "Arik",
+        "content": commentText
+      }
     );
   }
 
-  initializeForm():void {
-    this.createDocumentForm = this.fb.group({
-      name : [null, Validators.required],
-      type : [null, Validators.required],
-      file : [null, Validators.required],
-      school: [null],
-      department: [null],
-      // todo: max course length if you set it yourself?
-      course: [null],
-      description : [null, Validators.maxLength(500)],
-      tags : this.fb.array([new FormControl()])
-    });
-  }
-
-  addTag(): void {
-    (this.createDocumentForm.get('tags') as FormArray).push(new FormControl());
-  }
-
-  removeTag(i : number): void {
-    (this.createDocumentForm.get('tags') as FormArray).removeAt(i);
-  }
-
-  pruneArray(array: string[]): string[] {
-
-    let arrayCleaned: string[] = [];
-
-    if (array == null || array == undefined || array.length == 0) {
-      return arrayCleaned;
-    }
-
-    array.forEach(tag => {
-      if (tag != undefined && tag != null && tag.trim() != '') {
-        arrayCleaned.push(tag.trim().toUpperCase());
-      }
-    });
-
-    return arrayCleaned.filter((tag, index, arrayCleaned) => {
-      if (arrayCleaned.indexOf(tag) == index) {
-        return tag;
-      }
-    });
+  calculateNewRating(newRating: number) {
+    this.defineRatingForDocumentDetail(newRating);
+    this.userGaveRating = true;
   }
 
 }
+
 
---
+++ gutenshare-web/src/main/java/com/group4/gutenshareweb/infrastructure/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.group4.gutenshareweb.infrastructure.controller;
+
+import com.group4.api.DocumentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/Login")
+public class LoginController {
+
+
+
+
+}
