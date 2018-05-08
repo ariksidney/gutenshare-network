0654d9486ea10e585aa706b40cbcdb59df92e94f
rueegali <rueegali@students.zhaw.ch>
2018-05-08 08:50:48 +0200

    add search service

--- gutenshare-frontend/src/app/document-browser/document-browser.service.ts
+++ gutenshare-frontend/src/app/document-browser/document-browser.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { ErrorObservable } from "rxjs/observable/ErrorObservable";
 import { catchError } from "rxjs/operators";
-import {DocumentBrowser} from "./document-browser";
+import { DocumentBrowser } from "./document-browser";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -24,9 +24,15 @@ export class DocumentBrowserService {
   }
 
   searchDocuments (q: string) {
-    return this.http.get("/api/search",
+    var blub = this.http.get("http://192.168.1.43:8080/api/search",
       {params: {
         query: q
       }})
+      .subscribe((res: Response) => {
+      var data = res.json();
+        console.log(data);
+
+    });
   }
+
 }
