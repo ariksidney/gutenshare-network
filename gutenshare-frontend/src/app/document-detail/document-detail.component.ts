14ff23dc00ec3d932d497aef78486073630e93fe
Arik Sidney Guggenheim <guggeari@students.zhaw.ch>
2018-05-10 20:56:11 +0200

    added document download

--- gutenshare-frontend/src/app/document-detail/document-detail.component.ts
+++ gutenshare-frontend/src/app/document-detail/document-detail.component.ts
@@ -11,6 +11,7 @@ import {DocumentDetail} from "./document-detail";
   styleUrls: ['./document-detail.component.scss']
 })
 
+
 export class DocumentDetailComponent implements OnInit {
   response;
   specificDocument = new DocumentDetail();
@@ -27,7 +28,6 @@ export class DocumentDetailComponent implements OnInit {
 
         this.mapResponseToSpecificDocument(response);
 
-
         console.log(response);
 
         this.defineRatingForDocumentDetail(this.specificDocument.rating);
@@ -40,8 +40,7 @@ export class DocumentDetailComponent implements OnInit {
     documentComment.user = 'rudi';
     documentComment.content = commentText;
 
-    this.apiService.postComment(documentComment).then(data =>
-      {
+    this.apiService.postComment(documentComment).then(data => {
         this.apiService.getDocumentDetails(this.route.snapshot.params.id)
           .then(response => {
             this.mapResponseToSpecificDocument(response);
@@ -57,8 +56,7 @@ export class DocumentDetailComponent implements OnInit {
     documentReview.user = 'rudi';
     documentReview.rating = newRating;
 
-    this.apiService.postRating(documentReview).then(data =>
-      {
+    this.apiService.postRating(documentReview).then(data => {
         this.apiService.getDocumentDetails(this.route.snapshot.params.id)
           .then(response => {
             this.mapResponseToSpecificDocument(response);
@@ -70,7 +68,7 @@ export class DocumentDetailComponent implements OnInit {
     );
   }
 
-  private mapResponseToSpecificDocument(response: any){
+  private mapResponseToSpecificDocument(response: any) {
     this.specificDocument.id = this.response.id;
     this.specificDocument.title = this.response.title;
     this.specificDocument.user = this.response.user;
@@ -102,6 +100,49 @@ export class DocumentDetailComponent implements OnInit {
 
   private refactorDate(date: string[]): string {
     return this.specificDocument.uploadDate = `${date[2]}.${date[1]}.${date[0]}`
+  }
+
+  public downloadFile(): void {
+    var a = document.createElement('a');
+    var t = atob(this.response.documentAsBytes);
+    var byteNumbers = new Array(t.length);
+    for (var i = 0; i < t.length; i++) {
+      byteNumbers[i] = t.charCodeAt(i);
+    }
+    var byteArray = new Uint8Array(byteNumbers);
+    let blob = new Blob([byteArray], {
+      type: this.getMimeType(this.response.fileType)
+    });
+    let url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = this.response.title + '.' + this.response.fileType;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
+  private getMimeType(extension:string):string {
+    if (extension === 'pdf') {
+      return 'application/pdf';
+    } else if (extension === 'doc') {
+      return 'application/msword';
+    } else if (extension === 'docx') {
+      return 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    } else if (extension === 'txt') {
+      return 'text/plain';
+    } else if (extension === 'jpg' || extension === 'jpeg') {
+      return 'image/jpeg'
+    } else if (extension === 'png') {
+      return 'image/png';
+    } else if (extension === 'md') {
+      return 'text/markdown';
+    } else if (extension === 'ppt') {
+      return 'application/mspowerpoint';
+    } else if (extension === 'pptx') {
+      return 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+    }
+    return '';
   }
 }
 
